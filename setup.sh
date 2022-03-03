3cc51464695313aebd8e0def73e970af3a805fe7
Michael Seeber <msr@financial.com>
2022-03-03 13:40:25 +0100

    another testcommit

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Add post-checkout script that will decrypt everything when checking out
 cat <<EOF > .git/hooks/post-checkout
 #!/bin/bash
 
@@ -7,6 +8,7 @@ python main.py -d -k \$(pass show production/certs_encryption_key) -f "*"
 
 EOF
 
+# Add pre-commit script that will encrypt everything when commting
 cat <<EOF > .git/hooks/pre-commit
 #!/bin/bash
 
@@ -14,5 +16,9 @@ python main.py -e -k \$(pass show production/certs_encryption_key) -f "*"
 git add -A
 EOF
 
+# Grant executions rights on these scripts
 chmod +x .git/hooks/post-checkout
 chmod +x .git/hooks/pre-commit
+
+# Checkout the main branch, to decrypt everything
+git checkout main
